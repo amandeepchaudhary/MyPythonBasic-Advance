2604ce3c8a162cafe6c813458178e8b647763191
Amandeep Singh <amanchaudhary.ac1@gmail.com>
2022-09-14 07:56:08 +0530

    Importing Tech.work to aman after user.rq in work

--- SMSPackageTwentySixthDay/aman.py
+++ SMSPackageTwentySixthDay/aman.py
@@ -2,7 +2,10 @@
 
 import Admin.service
 import Admin.Common.footer
+import Tech.work
 
 Admin.Common.footer.admin_common_footer()
-
 Admin.service.admin_service()
+Tech.work.tech_work()
+print(Tech.work.a)
+print(Tech.work.b)
