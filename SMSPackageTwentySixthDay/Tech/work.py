959939807545317ec650a524fc68640b784e3a0a
Amandeep Singh <amanchaudhary.ac1@gmail.com>
2022-09-14 07:54:24 +0530

    Cheacking for output and Alternate method

--- SMSPackageTwentySixthDay/Tech/work.py
+++ SMSPackageTwentySixthDay/Tech/work.py
@@ -5,11 +5,14 @@
 # work module
 import User.request
 
+a = "aman"
 def tech_work():
     print(" Tech package ---> work module")
     print("tech_work module Function")
     print()
-    User.request.user_request()
+    
+User.request.user_request()
+b = "aman"
 
 #for accessing the module file in tech work we need to see the tech.work file as a script so we
 #use a flag named -m(mod) = which runs a module as a script so, we run this where we can see the both siblings means both the folders/packages in file explorer ,C:/users/ishu/desktop/pythonproject/smspackage= python -m Tech.work
@@ -17,3 +20,11 @@ def tech_work():
 #we have a altternative as hack who don't know how to do this
 #import sys
 # sys.path.append('users package path==== c:/users/ishu/desktop/pythonproject/smspackage/user'), import request , request.user_request()
+#this runs in Tech package location
+
+# <<<<<<<<<<<<<<<<OR>>>>>>>>>>>>>>>>>
+
+import sys
+sys.path.append('C:/Users/ISHU/Desktop/PythonProject/SMSPackageTwentySixthDay/User')
+import request
+request.user_request()
