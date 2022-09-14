c686051dfc76fc1a3e69539a58e951cf52670e63
Amandeep Singh <amanchaudhary.ac1@gmail.com>
2022-09-14 06:42:44 +0530

    imported coomon.footer in aman

--- SMSPackageTwentySixthDay/Tech/work.py
+++ SMSPackageTwentySixthDay/Tech/work.py
@@ -1,5 +1,6 @@
 # Tech package ---> work module
 
+#Siblings concept == means 
 def tech_work():
     print(" Tech package ---> work module")
     print("tech_work module Function")
--- SMSPackageTwentySixthDay/aman.py
+++ SMSPackageTwentySixthDay/aman.py
@@ -1,6 +1,8 @@
 # <<<<<<aman module>>>>>> 
 
 import Admin.service
+import Admin.Common.footer
 
+Admin.Common.footer.admin_common_footer()
 
 Admin.service.admin_service()
