7eead393928e4795a6b28dfb333c7c3ce7e29b4e
Amandeep Singh <amanchaudhary.ac1@gmail.com>
2022-09-15 02:05:31 +0530

    temp

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,2 +1,10 @@
-def __add__(self,other):
-    
+from datetime import date
+dob=date(2000,8,22)
+
+t=date.today()
+print(t)
+age=t.year - dob.year-((t.month, t.day)<(dob.month,dob.day))
+print(age)
+
+print(2-False)
+print(2-True)
