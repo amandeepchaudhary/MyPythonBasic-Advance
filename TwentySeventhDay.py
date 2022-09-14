cc40ddaa778e7cfa0328174c2468692072c92fab
Amandeep Singh <amanchaudhary.ac1@gmail.com>
2022-09-15 02:06:47 +0530

    Abstract,Interface,time,sleep,calculating age

--- TwentySeventhDay.py
+++ TwentySeventhDay.py
@@ -131,9 +131,141 @@ class GrandChild(Child):
 gc=GrandChild()
 gc.disp1()
 gc.disp2()
+print()
 
 #abstract class have both concrete and abstract methods, whereas in interface only abstract methods are there
 #we use abstract class when some common features are used by all classes, whereas we use interface when all features need to be implemented differently for different objects
 #its a programers job to make a child class for an abstract class, but in interface any third party vendor will take resposibility to make child class
 
 # Interface are slow Compared to Abstract Class
+
+
+
+#Time Module
+from time import time, ctime, localtime
+
+epoch = time()
+print(epoch)
+
+ct=ctime(epoch)
+print(ct)
+
+stobj=localtime()
+print(stobj)
+#we can access seperate data form localtime>>>>>>>
+
+print(f"Year:{stobj.tm_year}",end="/ ")
+print(f"Year Day:{stobj.tm_yday}",end="/ ")
+print(f"Month Number:{stobj.tm_mon}",end="/ ")
+print(f"Week day:{stobj.tm_wday}",end="/ ")
+print(f"Month day:{stobj.tm_mday}",end="/ ")
+print(f"Hours now:{stobj.tm_hour}",end="/ ")
+print(f"Minutes now:{stobj.tm_min}",end="/ ")
+print(f"Seconds now:{stobj.tm_sec}")
+
+#datetime
+from datetime import datetime
+dt1 = datetime(year=2022,month=9,day=14)
+dt2 = datetime(year=2022,month=9,day=14,hour=23,minute=59)
+dt3 = datetime(2022,9,24)
+dt4 = datetime(2022,9,14,00,1)
+
+print(dt1)
+print(dt2)
+print(dt3)
+print(dt4)
+
+dtn=datetime.now()
+print(dtn)
+
+from datetime import date
+d1 = date(year=2022, month=9, day=30)
+d2 = date(2022,9,3)
+print(d1)
+print(d2)
+print(d1.year)
+
+cd1=date.today()
+print(cd1)
+
+from datetime import time
+t43=time(hour=23,minute=34,second=45)
+t43=time(23,34,59)
+print(t43.hour)
+print(t43)
+
+#timedelta class>>>>>>>>>>>
+#used to get a previous and Future dates
+#we can use this in Subscriptions as user can see how many days are there for him to enjoy his/her subscription
+from datetime import timedelta,date
+td=timedelta(days=30)
+d=date.today()  #can get todays date from date.today &
+print(f"Date of Subscription:{d}")
+print(f"Subscription End date:{d+td}")     #can get date after 30 days from timedelta
+print(d-td)
+
+i=31
+print(type(i))
+while i>0:
+    print("Days Remaining for the subscription:",i)
+    i-=1
+print("Subscription is Over! Recharge to Enjoy services")
+
+#Comparing two dates>>>>>>
+from datetime import date 
+d123=date(2022,3,22)
+d234=date(2019,9,8)
+print(d123==d234)
+print(d123<d234)
+print(d123>d234)
+print(d123!=d234)
+
+#Formatting date and time>>>>>>>>>>>>>>>
+from datetime import datetime
+d=datetime.today()
+print(d)
+print(type(d))
+print()
+
+a11=d.strftime("%m/%d/%y")
+print(a11)
+print(type(a11))
+print()
+
+a12=d.strftime("%B %d, %Y")
+print(a12)
+print()
+
+a13=d.strftime("%d-%b-%y")
+print(a13)
+print()
+
+a14=d.strftime("%H:%M:%S")  #gives hour in 24 hour Format
+print(a14)
+print()
+
+a15=d.strftime("%I:%M:%S")  #Gives hour in 12 Hour Format
+print(a15)
+print()
+
+#sleep() method = is used to stop execution of a program temporarily for a given amount of time. When this function  is called, PVM stops program execution for given amount of time
+#It belongs to time module>>>>>>>>>>>>>>>
+
+import time
+
+for i in range(20): #in 0 sleep for 0 seconds and in 20 sleep for 20 Seconds
+    print(i)
+    if(i<20):
+        time.sleep(i)
+
+#calculating age 
+from datetime import date
+dob=date(2000,8,22)
+
+t=date.today()
+print(t)
+age=t.year - dob.year-((t.month, t.day)<(dob.month,dob.day))
+print(age)
+
+print(2-False) #False is Zero(0)
+print(2-True)  #True is One(1)
