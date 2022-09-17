a1aca6790efde5b91f1fc10dbf856fa6dd784836
Amandeep Singh <amanchaudhary.ac1@gmail.com>
2022-09-17 20:52:24 +0530

    tell(),seek(),r+,w+,a+,ab+,wb+,rb+,Copy,with

--- ThirtiethDay.py
+++ ThirtiethDay.py
@@ -96,6 +96,87 @@ for i in m1:
 print(len(m1))
 f.close()
 print("Completed the Reading!!!!!!")
+print()
 
-#tell() and seek() Method==========>>>>>>>>>>>>>>
-f=open('AmanFileHandling.txt')
+#tell() Method = tell the location of the cursor in the file  and seek() Method = is used set the cursor at the needed location ==========>>>>>>>>>>>>>>
+f=open('AmanFileHandling.txt', mode='r')
+print(f.tell()) #location of the cursor is at the starting
+data1=f.read(5) #after this the location of the cursor is at the 5th location
+print(data1)
+print(f.tell()) #shows the location 5 cause the cursor is at 5th
+data2=f.read(14)
+print(data2)
+print(f.tell()) #the location of the cursor is at 19th
+f.close()
+print()
+
+# p='nona non'
+# print(p.title())
+
+
+#seek(position)==
+f=open('AmanFileHandling.txt',mode='r')
+print(f.tell())
+f.seek(5)
+print(f.tell())
+f.close()
+print()
+
+#r+,w+,a+ == Similar in rb+,wb+,ab+
+#r+ = first read then write
+f=open('AmanFileHandling.txt',mode='r+')
+print(f.tell())
+print(f.read())
+print(f.tell())
+f.write('Yoooo Boiiii!!!!!!!!')
+print(f.tell())
+f.close()
+print()
+
+#w+=== first write then read but with the help of seek method because after writing the cursor will be at the end and then read method comes so it eill be blank
+f=open('AmanFileHandling.txt',mode='w+')
+print(f.tell())
+f.write('Yoooo Boiiii!!!!!!!!')
+print(f.tell())
+f.seek(0)
+print(f.tell())
+print(f.read())
+print(f.tell())
+f.close()
+print()
+
+#a+ = first append then read with the help of seek method
+f=open('AmanFileHandling.txt',mode='a+')
+print(f.tell()) #will show at the end of the previous text
+f.write('NOOOOOOOOOOOOOOOOOOOOOOO Yoooo Boiiii!!!!!!!!')
+print(f.tell())
+f.seek(0)
+print(f.tell()) 
+print(f.read())
+print(f.tell())
+f.close()
+print()
+
+#How to copy File Contents in python>>>>>>>>>>>>>>>>>>
+f=open('AmanFileHandling.txt',mode='r') #Existing File
+data=f.read()
+print("***********************************************")
+f1=open('AmanCopyFileHandling.txt',mode='w+')    #Copied File
+f1.write(data)
+f1.seek(0)
+cdata=f1.read()
+print(cdata)
+f.close()
+f1.close()
+print()
+
+#With Statement = with statement automatically Closed a open statement and used while opening a file>>>>>>>>>>>>>>>>>>>>>>>
+# syntax:=====
+# with open('file_name',mode='r') as file_obj:
+#     statements like read write append exclusive and all
+
+with open('AmanFileHandling.txt',mode='r') as f1:
+    data=f1.read()
+    print(data)
+    print(f1.closed)
+print(f1.closed)
